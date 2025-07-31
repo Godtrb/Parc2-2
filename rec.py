c1832e7fdec4a78141ed677592aa00541efd41d5
GodT <Godtrb@Gmail.com>
2025-07-30 19:13:03 -0600

    OH GOD HELP ME

--- rec.py
+++ rec.py
@@ -24,26 +24,37 @@ def ContLetra(a,pal,len,cont=0):
     if len==0:
         return cont
     else:
-        if pal[len]==a:
+        if pal[len-1]==a:
             cont+=1
             return ContLetra(a,pal,len-1,cont)
         else:
             return ContLetra(a,pal,len-1,cont)
-match e:
-    case 1:
-        firstimp=int(input("Enter first number: "))
-        secondimp=int(input("Enter second number: "))
-        print(f"El MCD es {Euclid(firstimp,secondimp)} ")
-    case 2:
-        text=input("Enter text: ")
-        Reps=int(input("Enter number of times youd like the text to repeat: "))
-        RepText(text,Reps)
-    case 3:
-        numdigs=int(input("Enter number: "))
-        print(f"el numero ingresado tiene {DigCont(numdigs)} digitos")
-while e!=5:
+e=6
+
+
+while e!=6:
     print("------Menu Recursivo--------")
     print("1.MCD")
     print("2.Rep un texto varias veces")
     print("3.Contar digitos de un numero")
     print("4.Contar letras en una palabra")
+    print("5.Convertir a binario")
+    print("6.Exit")
+    e=int(input())
+    match e:
+        case 1:
+            firstimp = int(input("Ingrese el primer numero: "))
+            secondimp = int(input("Ingrese el segundo numero: "))
+            print(f"El MCD es {Euclid(firstimp, secondimp)} ")
+        case 2:
+            text = input("Ingrese texto: ")
+            Reps = int(input("ingrese el numero de veces a repetir el texto "))
+            RepText(text, Reps)
+        case 3:
+            numdigs = int(input("Enter number: "))
+            print(f"el numero ingresado tiene {DigCont(numdigs)} digitos")
+        case 4:
+            palreq = input("Ingrese palabra: ")
+            letra = input("Ingrese letra: ")
+            lenght = len(palreq)
+            print(f"La letra {letra} se repite {ContLetra(letra, palreq, lenght)} veces")
